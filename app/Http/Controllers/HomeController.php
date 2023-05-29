736915a6b7e109635f614ad78a955ee868d9db4e
Arnaldo Teruo <teruo.perez@gmail.com>
2023-05-29 12:11:43 +0900

    課題提出

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,9 +7,11 @@ use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
-	public function index()
+	public function index(Request $request)
 	{
-		$movies = Http::asJson()->get(config('services.tmdb.endpoint') . 'search/movie?api_key=' . config('services.tmdb.api') . '&query=ドラゴンボール');
+		$search = $request -> input('search');
+
+		$movies = Http::asJson()->get(config('services.tmdb.endpoint') . 'search/movie?api_key=' . config('services.tmdb.api') . '&query=' . urlencode($search));
 		$movies = isset($movies->json()['results']) ? $movies->json()['results'] : [];
 		return view('home', compact('movies'));
 	}
